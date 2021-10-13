fa939b9a3f1dd54d60f9628ae2b4ae8c9502f943
Pieter Provoost <pieterprovoost@gmail.com>
2021-10-13 12:12:04 +0000

    version 2.8.2

--- inst/doc/getting-started.R
+++ inst/doc/getting-started.R
@@ -1,5 +1,8 @@
 ## ---- include = FALSE---------------------------------------------------------
+NOT_CRAN <- identical(tolower(Sys.getenv("NOT_CRAN")), "true")
 knitr::opts_chunk$set(
+  purl = NOT_CRAN,
+  eval = NOT_CRAN,
   collapse = TRUE,
   comment = "#>"
 )
